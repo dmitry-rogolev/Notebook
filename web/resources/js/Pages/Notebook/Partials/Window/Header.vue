c7ff6088a2577ccc2d0f49ca140f0fb1c09ce1f9
dmitry <work.drogolev@internet.ru>
2023-06-18 17:29:35 +0700

    commit

--- web/resources/js/Pages/Notebook/Partials/Window/Header.vue
+++ web/resources/js/Pages/Notebook/Partials/Window/Header.vue
@@ -7,26 +7,22 @@
             class="flex-auto px-3 sm:px-4 md:px-5 py-2 bg-gray-50 dark:bg-slate-800 border-0 border-b border-r border-gray-300 dark:border-gray-600 focus:border-gray-300 dark:focus:border-gray-600 text-gray-900 dark:text-gray-100 text-xl focus:ring-0 focus-visible:outline-none" 
             placeholder="Enter the title" 
             />
-        
-        <button 
-            @click="$notebook.delete()" 
-            type="button" 
-            class="px-4 py-2 bg-gray-50 hover:bg-gray-100 dark:bg-slate-800 dark:hover:bg-slate-700 focus:bg-gray-100 dark:focus:bg-slate-700 border-b border-r border-gray-300 dark:border-gray-600 dark:focus:border-gray-600 text-gray-700 dark:text-gray-300 select-none focus-visible:outline-none transition duration-200 ease-in-out print:hidden"
-            >
-            <i class="fa-solid fa-trash"></i>
-        </button>
+
+        <ConfirmDeleteModalWindowPartial />
 
     </WindowHeaderComponent>
 </template>
 
 <script>
 import WindowHeaderComponent from '@/Plugins/Window/Components/WindowHeader.vue';
+import ConfirmDeleteModalWindowPartial from './Modals/ConfirmDelete.vue';
 
 export default {
     name: 'WindowHeaderPartial', 
 
     components: {
         WindowHeaderComponent, 
+        ConfirmDeleteModalWindowPartial, 
     },  
 }
 </script>
---
+++ web/resources/js/Pages/Notebook/Partials/Window/Modals/ConfirmDelete.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <TriggerComponent v-bind="$attrs" @click="modal.show();" class="px-4 py-2 h-full bg-gray-50 hover:bg-gray-100 dark:bg-slate-800 dark:hover:bg-slate-700 focus:bg-gray-100 dark:focus:bg-slate-700 border-b border-r border-gray-300 dark:border-gray-600 dark:focus:border-gray-600 text-gray-700 dark:text-gray-300 select-none focus-visible:outline-none transition duration-200 ease-in-out print:hidden">
+            <i class="fa-solid fa-trash"></i>
+        </TriggerComponent>
+        <teleport to="body">
+            <ModalComponent ref="modal" @close="modal.hide();">
+                <template #content>
+                    <div class="text-gray-700 dark:text-gray-300 text-xl text-center mb-2">Are you sure you want to delete this note?</div>
+                    <div class="flex justify-center items-center p-4">
+                        <button 
+                            type="button"
+                            role="button"
+                            class="block mr-3 bg-red-600 hover:bg-red-700 border-red-600 hover:border-red-600 border-2 text-gray-100 focus-visible:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-lg shadow-md px-4 py-2 transition duration-300 ease-in-out"
+                            @click="$notebook.delete(); modal.hide();" 
+                            >
+                            Yes, I'm sure
+                        </button>
+                        <button 
+                            type="button"
+                            role="button"
+                            @click="modal.hide();" 
+                            class="block border-gray-300 dark:border-gray-600 border-2 rounded-lg shadow-md px-4 py-2 bg-white dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out focus-visible:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-600"
+                            >
+                            No, cancel
+                        </button>
+                    </div>
+                </template>
+            </ModalComponent>
+        </teleport>
+    </div>
+</template>
+
+<script>
+import ModalComponent from '@/Components/Modal/Modal.vue';
+import TriggerComponent from '@/Components/Modal/Trigger.vue';
+import { Modal } from 'flowbite';
+
+export default {
+    name: 'ConfirmDeleteModalWindowPartial', 
+
+    components: {
+        ModalComponent, 
+        TriggerComponent, 
+    }, 
+
+    data() {
+        return {
+            modal: null, 
+        };
+    },
+
+    methods: {
+        initModal() {
+            this.modal = new Modal(this.$refs.modal.$el, {
+                placement: 'center-center', 
+                backdrop: 'dynamic', 
+                closable: true, 
+            });
+        }, 
+    }, 
+
+    mounted() {
+        this.initModal();
+    }, 
+}
+</script>
+
+<style scoped>
+
+</style>
