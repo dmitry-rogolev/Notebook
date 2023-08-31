0ec6fb4e8c9f300a71849355d48fe4532122a580
dmitry <work.drogolev@internet.ru>
2023-08-31 22:01:55 +0700

    commit

--- web/resources/js/Pages/Notebook/Partials/Window/Tabs.vue
+++ web/resources/js/Pages/Notebook/Partials/Window/Tabs.vue
@@ -1,6 +1,7 @@
 <template>
     <WindowTabsComponent>
         <TabsComponent 
+            class="h-full"
             activeClasses="border-indigo-500 dark:border-indigo-600 text-indigo-500 dark:text-indigo-600 cursor-default pointer-events-none"
             inactiveClasses="border-gray-300 dark:border-gray-600 text-gray-700 dark:text-gray-300 hover:border-gray-400 dark:hover:border-gray-500 hover:text-gray-800 dark:hover:text-gray-200 focus:border-indigo-500 dark:focus:border-indigo-600 focus:text-indigo-500 dark:focus:text-indigo-600"
             triggersContainerClass="border-gray-300 dark:border-gray-600 border-b px-4 bg-gray-100 dark:bg-slate-800 text-lg md:text-sm lg:text-base overflow-x-auto"
